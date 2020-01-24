c429a55b622b52e06d4fa2996dc03461f951718a
franciscojmr <franciscojmr@meta4.com>
2020-01-24 12:02:54 +0100

    add mock module

--- __tests__/m4node.initialize.spec.ts
+++ __tests__/m4node.initialize.spec.ts
@@ -1,6 +1,9 @@
 import { M4ApiNode } from "./..//src/m4apiNode";
 
+jest.mock('require-from-url/sync', jest.fn())
+
 describe("Initialize M4Node.ts Suite", ()=>{
+
     test("Should create a M4ApiNode.ts instance", ()=> {
         const m4Node = new M4ApiNode("server","user","pass");
         expect(m4Node).toBeTruthy();
@@ -8,6 +11,14 @@ describe("Initialize M4Node.ts Suite", ()=>{
 
     test("Should throw an error when initialize is not success", (done)=> {
         const m4Node = new M4ApiNode("server","user","pass");
+        m4Node.initialize().catch((error: TypeError) => {
+            expect(error.message).toContain("Invalid URL")
+            done();
+        });
+    });
+
+    test("Should initialize success", (done)=> {
+        const m4Node = new M4ApiNode("http://franciscocaw10.meta4.com:5020/pene.js","user","pass");
         m4Node.initialize().catch((error: TypeError) => {
             expect(error.message).toContain("Invalid URL")
             done();
