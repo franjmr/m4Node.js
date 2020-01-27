057525e340b61cdba5985b418c7cd2caf2fe711a
franciscojmr <franciscojmr@meta4.com>
2020-01-27 17:44:15 +0100

    Add export const

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
+"use strict";
+
 import { M4ApiNode }  from './m4apiNode';
 
-const jsapiServer = 'http://franciscocaw10.meta4.com:5020/' ;
-const usr = "ORLIEMOBILE";
-const pwd = "RUN";
-const m4apiNode = new M4ApiNode(jsapiServer,usr,pwd);
+async function _initialize(server: string, user:string, pass:string) {
+    const m4apiNode = new M4ApiNode(server,user,pass);
+    m4apiNode.initialize().then(()=>{
+        return m4apiNode;
+    })
+}
 
-m4apiNode.initialize().then( async ()=>{
-    await m4apiNode.logonPromise();
-    await m4apiNode.loadMetadataPromise(['PSCO_WDG_MY_TASKS']);
-    const request = await m4apiNode.executeMethodPromise("PSCO_WDG_MY_TASKS", "PSCO_WDG_MY_TASKS", "PLCO_LOAD", [null]);
-    const objRequest = request.getObject();
-    if( objRequest ){
-        const nodeRequest = objRequest.getNode("PSCO_WDG_FUNC_PROCESSES");
-        console.log("Method executed ok! Number of records is: " + nodeRequest.count());
-    }
-    await m4apiNode.logoutPromise();
-    console.log("All done!")
-});
+export const initialize = _initialize;
