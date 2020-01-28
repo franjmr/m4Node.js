2834c8c666e0c2b1f9de2207d94878a79753e7fa
franciscojmr <franciscojmr@meta4.com>
2020-01-28 14:54:06 +0100

    Fix scripts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { M4ApiNode }  from './m4apiNode';
 
 async function _initialize(server: string, user:string, pass:string) {
     const _m4apiNode = new M4ApiNode(server,user,pass);
-    const _initializedInstance = await _m4apiNode.initializeAsync();
-    return _initializedInstance;
+    await _m4apiNode.initializeAsync();
+    return _m4apiNode;
 }
 
-export const m4ApiNode = _initialize;
+export const M4ApiNodejs = _initialize;
